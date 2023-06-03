eb6c54f5ba762b2e5356db680ef453ff7aba2123
giwrgoskai1988 <giwrgoskai1988@gmail.com>
2023-06-03 15:42:12 +0300

    Prettier

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,24 +3,28 @@ import axios from "axios";
 const getUrl = (url) => `https://localhost:4000/${url}`;
 
 const request = ({ method, url, params, data = {} }) => {
-    let appUrl = getUrl(url);
+  let appUrl = getUrl(url);
+  const options = {
+    url: appUrl,
+    method,
+    params,
+    data,
+  };
 
-    const options = {
-        url: appUrl,
-        method,
-        params,
-        data
-    };
-
-    return axios.request(options);
-}
+  return axios.request(options);
+};
 
 export const get = ({ url, params }) => request({ method: "get", url, params });
 
-export const post = ({ url, params, data }) => request({ method: "post", url, params, data });
+export const post = ({ url, params, data }) =>
+  request({ method: "post", url, params, data });
 
-export const patch = ({ url, params, data }) => request({ method: "patch", url, params, data });
+export const patch = ({ url, params, data }) =>
+  request({ method: "patch", url, params, data });
 
-export const put = ({ url, params, data }) => request({ method: "put", url, params, data });
+export const put = ({ url, params, data }) =>
+  request({ method: "put", url, params, data });
 
-export const deleteRequest = ({ url, params, data }) => request({ method: "delete", url, params, data });
+export const deleteRequest = ({ url, params, data }) =>
+  request({ method: "delete", url, params, data });
+
